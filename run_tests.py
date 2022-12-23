ae361829b216bd66e6d48f9dae10af44ece8c7a6
J. Foederer <32476108+JFoederer@users.noreply.github.com>
2022-12-23 10:28:08 +0100

    Clean up run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -3,18 +3,18 @@ import sys
 import unittest
 import robot
 
-# Usage: python run_tests.py [utest] [atest]
-#
-#        When started without arguments, runs all unit and acceptance tests
-#        In place of 'atest' any set of Robot Framework command line options can be used
-#        Unit tests can also be executed by running their individually files as script
-
 if __name__ == '__main__':
+    if '-h' in sys.argv or '--help' in sys.argv:
+        print("""Usage: python run_tests.py [utest] [atest]
+        - When started without arguments, runs all unit and acceptance tests
+        - In place of 'atest' any set of Robot Framework command line options can be used
+        - Unit tests can also be executed by running their individual files as script""")
+        sys.exit(0)
     THIS_DIR = os.path.dirname(os.path.abspath(__file__))
     utest = False
     atest = True
     if len(sys.argv) == 1:
-        print("No test folder provided. Running all unit and acceptance tests from", THIS_DIR)
+        print("No arguments provided. Running all unit and acceptance tests from", THIS_DIR)
         sys.argv.append(os.path.join(THIS_DIR, 'atest'))
         utest = True
 
@@ -24,8 +24,10 @@ if __name__ == '__main__':
         atest = False if len(sys.argv) == 1 else True
 
     if utest:
-        urun = unittest.main(module=None, argv=[__file__, 'discover', os.path.join(THIS_DIR, 'utest')], exit=False)
-        if not urun.result.wasSuccessful():
+        utestrun = unittest.main(module=None,
+                             argv=[__file__, 'discover', os.path.join(THIS_DIR, 'utest')],
+                             exit=False)
+        if not utestrun.result.wasSuccessful():
             sys.exit(1)
 
     if 'atest' in sys.argv:
@@ -34,10 +36,11 @@ if __name__ == '__main__':
 
     if atest:
         OUTPUT_ROOT = os.path.join(THIS_DIR, 'atest', 'results')
-            
         # Adding the robotframeworkMBT folder to the python path forces the development
         # version to be used instead of the one installed on your system. You will also
         # need to add this path to your IDE options when running from there.
         robot.run_cli(['--outputdir', OUTPUT_ROOT,
                        '--pythonpath', THIS_DIR]
-                       + sys.argv[1:])
+                       + sys.argv[1:], exit=False)
+        if utest:
+            print(f"Also ran {utestrun.result.testsRun} unit tests")
