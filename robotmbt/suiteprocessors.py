d1619bd296aefa3e857390062f16c4d5c4cef34a
tychodub <t.b.dubbeling@student.utwente.nl>
2025-12-17 11:01:43 +0100

    implemented comments from Douwe (added comments and reset graph setting in suiteprocessors.py)

--- robotmbt/suiteprocessors.py
+++ robotmbt/suiteprocessors.py
@@ -88,7 +88,7 @@ class SuiteProcessors:
         out_suite.suites = []
         return out_suite
 
-    def process_test_suite(self, in_suite: Suite, *, seed: any = 'new', graph: str = 'reduced-sdv') -> Suite:
+    def process_test_suite(self, in_suite: Suite, *, seed: any = 'new', graph: str = '') -> Suite:
         self.out_suite = Suite(in_suite.name)
         self.out_suite.filename = in_suite.filename
         self.out_suite.parent = in_suite.parent
