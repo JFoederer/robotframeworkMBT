a1cfc5999ce4f1a9497a753acae2745057e8c207
Diogo Silva <diogo.xuya@gmail.com>
2025-12-17 10:27:15 +0000

    Full Screen + Graph Title (#37)
    * Make HTML generation static
    
    * Added a fullscreen button in the graph visualization toolbar
    
    * Added the suite name as the graph title
    
    * Fixed generate_html method to use NetworkVisualiser class
    
    * Changed generate_visualisation return to use GRAPH_SIZE_PX constant
    
    ---------
    
    Co-authored-by: Thomas Kas <t.s.kas@student.utwente.nl>

--- robotmbt/visualise/visualiser.py
+++ robotmbt/visualise/visualiser.py
@@ -2,7 +2,7 @@ from robotmbt.modelspace import ModelSpace
 from robotmbt.tracestate import TraceState
 from robotmbt.visualise.graphs.reducedSDVgraph import ReducedSDVGraph
 from robotmbt.visualise.graphs.scenariodeltavaluegraph import ScenarioDeltaValueGraph
-from robotmbt.visualise.networkvisualiser import NetworkVisualiser
+from robotmbt.visualise import networkvisualiser
 from robotmbt.visualise.graphs.abstractgraph import AbstractGraph
 from robotmbt.visualise.graphs.scenariograph import ScenarioGraph
 from robotmbt.visualise.graphs.stategraph import StateGraph
@@ -24,12 +24,14 @@ class Visualiser:
         # just calls __init__, but without having underscores etc.
         return cls(graph_type)
 
-    def __init__(self, graph_type: str):
+    def __init__(self, graph_type: str, suite_name: str = ""):
         if graph_type != 'scenario' and graph_type != 'state' and graph_type != 'scenario-state' \
                 and graph_type != 'scenario-delta-value' and graph_type != 'reduced-sdv':
             raise ValueError(f"Unknown graph type: {graph_type}!")
+
         self.graph_type: str = graph_type
         self.trace_info: TraceInfo = TraceInfo()
+        self.suite_name = suite_name
 
     def update_trace(self, trace: TraceState, state: ModelSpace):
         if len(trace.get_trace()) > 0:
@@ -48,8 +50,10 @@ class Visualiser:
             graph: AbstractGraph = ReducedSDVGraph(self.trace_info)
         else:
             graph: AbstractGraph = ScenarioStateGraph(self.trace_info)
-        html_bokeh = NetworkVisualiser(graph).generate_html()
-        return f"<iframe srcdoc=\"{html.escape(html_bokeh)}\", \
-            width=\"{NetworkVisualiser.GRAPH_SIZE_PX}px\", \
-            height=\"{NetworkVisualiser.GRAPH_SIZE_PX}px\">\
-            </iframe>"
+        
+        vis = networkvisualiser.NetworkVisualiser(graph, self.suite_name)
+        html_bokeh = vis.generate_html()
+        
+        graph_size = networkvisualiser.NetworkVisualiser.GRAPH_SIZE_PX
+        
+        return f'<iframe srcdoc="{html.escape(html_bokeh)}" width="{graph_size}px" height="{graph_size}px"></iframe>'
