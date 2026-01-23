3733ba2c2a27fd1e379a2c24db729728858c4221
Thomas <t.s.kas@student.utwente.nl>
2026-01-23 17:26:21 +0100

    Reduce update visualisation calls

--- robotmbt/suiteprocessors.py
+++ robotmbt/suiteprocessors.py
@@ -150,23 +150,19 @@ class SuiteProcessors:
         tracestate = TraceState(self.shuffled)
         while not tracestate.coverage_reached():
             candidate_id = tracestate.next_candidate(retry=allow_duplicate_scenarios)
-            self.__update_visualisation(tracestate)
             if candidate_id is None:  # No more candidates remaining for this level
                 if not tracestate.can_rewind():
                     break
                 tail = modeller.rewind(tracestate)
                 logger.debug(f"Having to roll back up to {tail.scenario.name if tail else 'the beginning'}")
                 self._report_tracestate_to_user(tracestate)
-                self.__update_visualisation(tracestate)
             else:
                 candidate = self._select_scenario_variant(candidate_id, tracestate)
                 if not candidate:  # No valid variant available in the current state
                     tracestate.reject_scenario(candidate_id)
-                    self.__update_visualisation(tracestate)
                     continue
                 previous_len = len(tracestate)
                 modeller.try_to_fit_in_scenario(candidate, tracestate)
-                self.__update_visualisation(tracestate)
                 self._report_tracestate_to_user(tracestate)
                 if len(tracestate) > previous_len:
                     logger.debug(f"last state:\n{tracestate.model.get_status_text()}")
@@ -174,14 +170,13 @@ class SuiteProcessors:
                     if self.__last_candidate_changed_nothing(tracestate):
                         logger.debug("Repeated scenario did not change the model's state. Stop trying.")
                         modeller.rewind(tracestate)
-                        self.__update_visualisation(tracestate)
                     elif tracestate.coverage_drought > self.DROUGHT_LIMIT:
                         logger.debug(f"Went too long without new coverage (>{self.DROUGHT_LIMIT}x). "
                                      "Roll back to last coverage increase and try something else.")
                         modeller.rewind(tracestate, drought_recovery=True)
-                        self.__update_visualisation(tracestate)
                         self._report_tracestate_to_user(tracestate)
                         logger.debug(f"last state:\n{tracestate.model.get_status_text()}")
+            self.__update_visualisation(tracestate)
         return tracestate
 
     def __update_visualisation(self, tracestate: TraceState):
