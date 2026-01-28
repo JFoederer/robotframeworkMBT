88fd99a40224682a93810b81beb81c4e23ccf12d
jonathan <148167.jw@gmail.com>
2026-01-28 11:01:16 +0100

    add licenses to files

--- robotmbt/visualise/networkvisualiser.py
+++ robotmbt/visualise/networkvisualiser.py
@@ -1,3 +1,33 @@
+# BSD 3-Clause License
+#
+# Copyright (c) 2026, T.B. Dubbeling,  J. Foederer, T.S. Kas, D.R. Osinga, D.F. Serra e Silva, J.C. Willegers
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 from bokeh.core.enums import PlaceType, LegendLocationType
 from bokeh.core.property.vectorization import value
 from bokeh.embed import file_html
@@ -226,10 +256,13 @@ class NetworkVisualiser:
         sugiyama = SugiyamaLayout(g.C[0])
 
         # Set specific margins as these values worked best in user-testing
-        sugiyama.xspace = 10
+        # Space between nodes
+        sugiyama.xspace = 10    
         sugiyama.yspace = 15
-        sugiyama.dw = 2
-        sugiyama.dh = 2
+        # Default width for nodes with no set width   (in this case only for edge routing)
+        sugiyama.dw = 2       
+        # Default height for nodes with no set height (in this case only for edge routing)  
+        sugiyama.dh = 2         
 
         sugiyama.init_all(roots=[start], inverted_edges=flips)
         sugiyama.draw()
